dac40a58846a1369ca05e29070c79ead7d60eb51
Alberto <alberto.tavares@zup.com.br>
2023-06-04 18:17:47 -0300

    ajustes finais que eu consigo enxergar

--- src/main/java/com/deveficiente/desafioformatamatriz/Solucao.java
+++ src/main/java/com/deveficiente/desafioformatamatriz/Solucao.java
@@ -26,11 +26,11 @@ public class Solucao {
         for(int posicao = 0; posicao < vetor.length; posicao++) {
             topo.append("+").append(caracteresParaTamanhoCelula);
 
+            
+            meio.append("|");
+            
             //lado direito foi gerado pelo copilot
             int diferenca = tamanhoCelula - String.valueOf(vetor[posicao]).length();
-
-            meio.append("|");
-
             for(int i = 0; i < diferenca; i++) {
                 meio.append(" ");
             }
@@ -40,36 +40,41 @@ public class Solucao {
             contadorLimiteColunas++;
 
             if(contadorLimiteColunas == limiteColunas) {
+                //fecha a linha
                 topo.append("+\n");
                 meio.append("|\n");                
                 resultado.append(topo).append(meio);
 
+                //reseta as variaveis (gerado pelo copilot)
                 topo = new StringBuilder();
                 meio = new StringBuilder();                
                 contadorLimiteColunas = 0;                
             }
         }
         
-        //aqui completa o topo com o que falta
-        for(int i = 0; i < limiteColunas - contadorLimiteColunas; i++) {
-            topo.append("+").append(caracteresParaTamanhoCelula);
-        }
 
-        //codigo gerado pelo copilot
-        //aqui fecha o topo e o meio
+        //codigo parcialmente gerado pelo copilot
+        //aqui fecha o topo e o meio quando a ultima linha está quebrada
         if(contadorLimiteColunas > 0) {
+
+            //aqui completa o topo da ultima linha com o que falta
+            for(int i = 0; i < limiteColunas - contadorLimiteColunas; i++) {
+                topo.append("+").append(caracteresParaTamanhoCelula);
+            }
+
             topo.append("+\n");
             meio.append("|\n");                
             resultado.append(topo).append(meio);
         }
         
         int resto = contadorLimiteColunas == 0 ? limiteColunas : contadorLimiteColunas;
-        //esse codigo o copilot pensou antes de mim
-        
+
+        //esse codigo o copilot pensou antes de mim        
         for(int i = 0; i < resto; i++) {
             fundo.append("+").append(caracteresParaTamanhoCelula);
         }
 
+        //fecha a última linha
         fundo.append("+");
 
         return resultado.append(fundo).toString();
